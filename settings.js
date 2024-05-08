7196182f76dee1380b65c0c0df3105927d064a0f
Dominican643 <152299619+Dominican643@users.noreply.github.com>
2024-05-08 17:31:53 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,13 +10,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "2347041620617,2347015667458"
+global.ownernomer = "2348052944641"
 global.ownername ="klaus mikealson"
 global.ytname = "YT: Xeon"
 global.socialm = "GitHub: Emperordagoat"
 global.location = "nigeria, lagos, abuja"
 
-global.ownernumber = '2347041620617,2347015667458'  //creator number
+global.ownernumber = '2348052944641'  //creator number
 global.ownername = 'klaus mikealson' //owner name
 global.botname = 'Emperor dee' //name of the bot
 
